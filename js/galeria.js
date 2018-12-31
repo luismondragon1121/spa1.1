80b32d0fee4d1a1c21b744de071538bff7b41dbc
unknown <luismondragon1121>
2018-12-31 12:44:27 -0600

    se trabajo en la seccion de galeria

--- js/galeria.js
+++ js/galeria.js
@@ -1,6 +1,8 @@
 $('.galeria__img').click(function(e){
     var img =e.target.src;
-    var modal = '<section class="container-fluid"><div class="row"><div class="modal__galeria" id="modal__galeria"><img src="'+ img +'" class="modal__img"><div class="modal__boton" id="modal__boton">X</div></div></div></section>'
+    var siguiente =$('#bt-next');
+    var anterior =$('#bt-prev');
+    var modal = '<section class="container-fluid"><div class="row"><div class="modal__galeria" id="modal__galeria"><img src="'+ img +'" class="modal__img"><div id="bt-prev">&#60;</div><div id="bt-next">&#62;</div><div class="modal__boton" id="modal__boton">X</div></div></div></section>'
     $('body').append(modal);
     $('#modal__boton').click(function(){
         $('#modal__galeria').remove();
