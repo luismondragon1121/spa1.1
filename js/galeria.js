1569ef98968949872575cc440e1e5984d467277d
unknown <luismondragon1121>
2019-01-03 13:42:38 -0600

    se realizaron pruebas en galeria

--- js/galeria.js
+++ js/galeria.js
@@ -1,10 +1,57 @@
 $('.galeria__img').click(function(e){
     var img =e.target.src;
-    var siguiente =$('#bt-next');
-    var anterior =$('#bt-prev');
-    var modal = '<section class="container-fluid"><div class="row"><div class="modal__galeria" id="modal__galeria"><img src="'+ img +'" class="modal__img"><div id="bt-prev">&#60;</div><div id="bt-next">&#62;</div><div class="modal__boton" id="modal__boton">X</div></div></div></section>'
+    var modal = '<section class="container-fluid"><div class="row"><div class="modal__galeria" id="modal__galeria"><img src="'+ img +'" class="modal__img" onclick="cambiar(this);"><div id="bt-prev">&#60;</div><div id="bt-next">&#62;</div><div class="modal__boton" id="modal__boton">X</div></div></div></section>'
     $('body').append(modal);
     $('#modal__boton').click(function(){
-        $('#modal__galeria').remove();
+        $('#modal__galeria').remove();  
+    })
+    $('#bt-next').click(function(e){
+       var imagen_old = $('modal__img').replaceWith('<img src="'+ img +'" class="modal__img">');
+		$('#modal__galeria').append(imagen_old);
     })
 })
+
+
+	// Array con todas las imágenes que deseamos que se vayan cambiando a
+	// cada clic
+	var imagenes=Array("img/galeria/1.jpg","img/galeria/2.jpg",
+     'img/galeria/3.jpg',
+     'img/galeria/6.jpg',
+     'img/galeria/7.jpg',
+     'img/galeria/8.jpg',
+     'img/galeria/13.jpg',
+     'img/galeria/14.jpg',
+     'img/galeria/16.jpg',
+     'img/galeria/19.jpg');
+	var imagenVisible=0;
+ 
+	// Función que cambia la imagen actual por la siguiente
+	function cambiar(img)
+	{
+		imagenVisible++;
+		if(imagenVisible>=imagenes.length)
+		{
+			imagenVisible=0;
+		}
+		img.src=imagenes[imagenVisible];
+		cargarSiguienteImagen();
+	}
+ 
+	/**
+	 * Esta función carga la siguiente imagen para no tener que esperar su carga
+	 * en el momento de mostrarla.
+	 */
+	function cargarSiguienteImagen()
+	{
+		if((imagenVisible+1)<imagenes.length)
+		{
+			console.log();
+			var imgTmp=new Image();
+			imgTmp.src=imagenes[imagenVisible+1];
+		}
+	}
+ 
+	window.onload=function() {
+		cargarSiguienteImagen();
+	}
+
--- js/preguntas.js
+++ js/preguntas.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-   $('ul.preres li a:first').addClass('active-spa');
+   $('ul.preres li a:first').addClass('');
     $('.aticulos-preguntas article').hide();
     $('.aticulos-preguntas article:first').show();
     
     $('ul.preres li a').click(function(){
-       $('ul.preres li a').removeClass('active-spa');
-        $(this).addClass('active-spa');
+       $('ul.preres li a').removeClass('');
+        $(this).addClass('');
         $('.aticulos-preguntas article').hide();
         
         var activeTab = $(this).attr('href');
