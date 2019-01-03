5fea09df4296f38bd8088efa95b53179fae4d0ca
unknown <luismondragon1121>
2019-01-03 16:27:00 -0600

    se modifico slider de carrusel al realizar pantalla de imagen en grande, se agrego efecto al pulsar la imagen

--- js/galeria.js
+++ js/galeria.js
@@ -1,6 +1,6 @@
 $('.galeria__img').click(function(e){
     var img =e.target.src;
-    var modal = '<section class="container-fluid"><div class="row"><div class="modal__galeria" id="modal__galeria"><img src="'+ img +'" class="modal__img" onclick="cambiar(this);"><div id="bt-prev">&#60;</div><div id="bt-next">&#62;</div><div class="modal__boton" id="modal__boton">X</div></div></div></section>'
+    var modal = '<section class="container-fluid"><div class="row"><div class="modal__galeria" id="modal__galeria"><img src="'+ img +'" class="modal__img" onclick="cambiar(this);"><div class="modal__boton" id="modal__boton">X</div></div></div></section>'
     $('body').append(modal);
     $('#modal__boton').click(function(){
         $('#modal__galeria').remove();  
