7e38aa9ddbb8641cdd056adbb1b731804a9dde7c
unknown <luismondragon1121>
2019-01-10 11:51:45 -0600

    se corrigio el inicio

--- js/spa.js
+++ js/spa.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-   $('.variety-selector > a:first').addClass('active-spa');
+   $('.cir_spa a:first').addClass('active');
     $('.circulos-secciones article').hide();
     $('.circulos-secciones article:first').show();
     
-    $('variety-selector > a').click(function(){
-       $('.variety-selector > a').removeClass('active-spa');
-        $(this).addClass('active-spa');
+    $('.cir_spa a').click(function(){
+       $('.cir_spa a').removeClass('active');
+        $(this).addClass('active');
         $('.circulos-secciones article').hide();
         
         var activeTab = $(this).attr('href');
         $(activeTab).show();
         return false;
     });
+    });
 
     $('.variety-selector > a , .spa-gallery-selectors > a').on('click',function(){
         $(this).addClass("active").siblings().removeClass('active');
-    });
     });
 
     // $(document).ready(function(){
