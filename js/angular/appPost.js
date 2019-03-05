0ff0907c5f6dddd812cbffd92f873276bd5d6bde
MIGUEL ANGEL MORENO <you@example.com>
2019-03-05 14:20:23 -0600

    creacion de seccion de tips

--- js/angular/appPost.js
+++ js/angular/appPost.js
@@ -1,4 +1,4 @@
-
+//controlador obtiene post
 var myApp = angular.module('app', ['angularUtils.directives.dirPagination','ngSanitize', 'ngMask']);
 
 myApp.controller('blog-spa', ['$scope', '$window', '$http', function($scope,$window,$http) {
@@ -12,10 +12,11 @@ $http.get('http://www.cscwellnesscenter.com.mx/blog-spa/?rest_route=/wp/v2/posts
 });
 }]);  
  
+//controlador obtiene tips
 myApp.controller('tips',['$scope', '$window','$http', function($scope,$window,$http) {
 
-$http.get('http://www.cscwellnesscenter.com.mx/blog-spa/?rest_route=/wp/v2/categories', {
-  headers: {'Authorization': 'basic' +btoa('user_noticias:DNHV$Dr0(*jJEg0uMV')}
+$http.get('http://www.cscwellnesscenter.com.mx/blog-spa/?rest_route=/wp/v2/posts&_embed&categories=5', {
+  headers: {'Authorization': 'basic' +btoa('user_spa:4N@i$%hbbJ@9EHESIQ')}
       
 }).then(function (res){
     $scope.postTips = res.data;
@@ -24,16 +25,6 @@ $http.get('http://www.cscwellnesscenter.com.mx/blog-spa/?rest_route=/wp/v2/categ
 }]);
 
 
-myApp.controller('entradasNotices', ['$scope','$window','$http', function($scope,$window,$http) {
-
-  $http.get('http://www.coine.lat/coine_noticias/?rest_route=/wp/v2/posts&_embed', {
-    headers: {'Authorization': 'Basic '+btoa('user_noticias:DNHV$Dr0(*jJEg0uMV') }
-   
-  }).then(function (res){
-    $scope.postNotices = res.data;
-    console.log($scope.postNotices);
-  });
-}]);
 
 
 
